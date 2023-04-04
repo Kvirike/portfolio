3fdeb7f87c123e231af2d2703727f6e912dcb614
Kvirike kemularia <kvirikekemularia@gmail.com>
2023-04-04 17:16:10 +0400

    drop down menu update

--- src/components/dropDownMenu.js
+++ src/components/dropDownMenu.js
@@ -1,11 +1,14 @@
 import '../App.scss'
 import { useTranslation } from "react-i18next";
+import menu from '../assets/img/portfolio/icons/menu.png'
 
 export default function DropMenu (){
     const { t } = useTranslation();
   return(
 <div className="dropdown">
-  <button className="dropbtn">{t('menu')}</button>
+  <button className="dropbtn">
+    <img src={menu}/>
+  </button>
   <div className="dropdown-content">
     <a className="nav-link" href="#about">
         {t('about')}
